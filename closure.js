6e3b54e2e4ebd5e8b3bcf3e6c2f41d1bf4ba68c7
Abraham Mejia <aimejia4@gmail.com>
2023-09-07 01:36:52 -0500

    Completed counter exercise

--- closure.js
+++ closure.js
@@ -40,28 +40,28 @@ console.log(count())
 console.log(count())
 
 
-// #2 Make Adder
+// // #2 Make Adder
 
-function makeAdder(num1){
-  return (num2) => {
-    return (num1 + num2)
-  }
-}
+// function makeAdder(num1){
+//   return (num2) => {
+//     return (num1 + num2)
+//   }
+// }
 
-console.log(makeAdder(2)(3)); //5
-console.log(makeAdder(3)(6)); //9
+// console.log(makeAdder(2)(3)); //5
+// console.log(makeAdder(3)(6)); //9
 
-// #3 ID Generator
+// // #3 ID Generator
 
-function idGenerator(){
-  let idAlpha = "AZ";
-  let idNum = 0;
-  return function (){
-    ++idNum;
-    return (idAlpha + idNum);
-  }
-}
-const generateId = idGenerator();
-console.log(generateId());
-console.log(generateId());
-console.log(generateId());
+// function idGenerator(){
+//   let idAlpha = "AZ";
+//   let idNum = 0;
+//   return function (){
+//     ++idNum;
+//     return (idAlpha + idNum);
+//   }
+// }
+// const generateId = idGenerator();
+// console.log(generateId());
+// console.log(generateId());
+// console.log(generateId());
